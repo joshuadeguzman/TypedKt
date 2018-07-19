58057431f17a5fc1be16e436aac64fb2cfdc99dd
dev-joshua <devjoshuadeguzman@gmail.com>
2018-07-19 15:00:00 +0800

    Update demo callbacks

--- app/src/main/java/io/jmdg/typedktapp/MainActivity.kt
+++ app/src/main/java/io/jmdg/typedktapp/MainActivity.kt
@@ -2,6 +2,7 @@ package io.jmdg.typedktapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -43,12 +44,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        // Resume animation
+        if(tkv_animate_xml.isAnimating()){
+            tkv_animate_xml.animateText()
+        }
+
+        if(tkv_character.isAnimating()){
+            tkv_character.animateText()
+        }
+
+        if(tkv_words.isAnimating()){
+            tkv_words.animateText()
+        }
+    }
+
     override fun onPause() {
         super.onPause()
-        // Remove callbacks
-        tkv_animate_xml.removeAnimation()
-        tkv_character.removeAnimation()
-        tkv_words.removeAnimation()
+        // Pause animation
+        tkv_animate_xml.skipAnimation()
+        tkv_character.skipAnimation()
+        tkv_words.skipAnimation()
     }
 
     override fun onDestroy() {
